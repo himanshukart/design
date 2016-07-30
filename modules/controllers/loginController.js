4d424031b3b2a7c6f7b59cfb58e268c4270ad632
himanshukart <himanshu.gupta@kartrocket.com>
2016-07-30 13:42:11 +0545

    angualr factories with controller~

--- modules/controllers/loginController.js
+++ modules/controllers/loginController.js
@@ -1,3 +1,13 @@
-engageApp.controller('loginController',['$scope','$http','$cookieStore','$location', function($scope,$http,$cookieStore,$location) {
+engageApp.controller('loginController',['$scope','$http','$location','userFactory', function($scope,$http,$location,userFactory) {
 
+  $scope.data = 'test';
+  userFactory.userLogin($scope.data)
+      .then(function onSuccess(response) {
+
+      })
+      .catch(function onError(sailsresponse) {
+        console.log(sailsresponse);
+      })
+      .finally(function eitherWay(){
+      })
 }]);
